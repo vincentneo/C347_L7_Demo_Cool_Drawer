cb21950954f8ade0099b1b3eaf20922d3abc1b9e
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-06-08 11:43:03 +0800

    section B

--- app/src/main/java/sg/edu/rp/c347/id19007966/democooldrawer/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/democooldrawer/MainActivity.java
@@ -1,13 +1,19 @@
 package sg.edu.rp.c347.id19007966.democooldrawer;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,6 +21,7 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActionBarDrawerToggle drawerToggle;
     private String[] drawerItems;
     private DrawerLayout drawerLayout;
     private ListView drawerList;
@@ -28,15 +35,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        currentTitle = this.getTitle().toString();
         drawerLayout = findViewById(R.id.drawerLayout);
         drawerList = findViewById(R.id.leftDrawer);
+        ab = getSupportActionBar();
 
         drawerItems = new String[] {"Bio", "Vaccination", "Anniversary"};
-        ab = getSupportActionBar();
 
         aa = new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1, drawerItems);
 
         drawerList.setAdapter(aa);
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                ab.setTitle("Make a selection");
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                ab.setTitle(currentTitle);
+            }
+        };
+
+        drawerLayout.addDrawerListener(drawerToggle);
+        ab.setDisplayHomeAsUpEnabled(true);
 
         drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -62,5 +87,25 @@ public class MainActivity extends AppCompatActivity {
                 drawerLayout.closeDrawer(drawerList);
             }
         });
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
